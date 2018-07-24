0e9a843fc04783ba590d4d16e92bcbd24107e102
Felipe Nicodemos <felipe.nicodemos@m2mrj.local>
2018-07-24 08:15:51 -0300

    remover credenciais

--- src/main/java/br/com/desafio/loadbalance/service/ServiceDefault.java
+++ src/main/java/br/com/desafio/loadbalance/service/ServiceDefault.java
@@ -1,5 +1,8 @@
 package br.com.desafio.loadbalance.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,14 @@ public class ServiceDefault {
 	
 	public void setRestTemplate(RestTemplateBuilder restTemplateBuilder) {
 		this.restTemplate = restTemplateBuilder.build();
+	}
+	
+	
+	private Map<String,String> autenticacaoAWS(){
+		Map<String,String> map = new HashMap<>();
+		map.put("X-Amz-Security-Token", "e3ad599e81dcc66d1d3bf149e1a8dd5e73de896764f5311761b56856163f284e");
+		
+		return map;
 	}
 	
 }
